c2fb5d05aff75deb70afac93bea2d4c2c4540871
paszin <pascalslogin@gmail.com>
2017-01-01 20:33:10 +0100

    integrated dynamo db close #1

--- backend/wiktionaryParser.py
+++ backend/wiktionaryParser.py
@@ -2,7 +2,34 @@
 import re
 import urllib2
 import json
+import boto3
 from HTMLParser import HTMLParser
+
+class DynamoDBConnector:
+
+    def  __init__(self):
+        dynamodb = boto3.resource('dynamodb')
+        self.table = dynamodb.Table('Dictionary')
+
+    def get(self, word):
+        item = self.table.get_item(
+            Key={
+            'word': word
+            },
+            AttributesToGet=['ipa']).get("Item")
+        if item:
+            return item.get("ipa")
+        else:
+            raise ValueError("Word not found")
+
+    def put(self, word, ipa):
+        item = self.table.put_item(
+            Item={
+            'word': word,
+            'ipa': ipa
+            })
+
+
 
 
 class WiktionaryIPAParser(HTMLParser):
@@ -58,22 +85,30 @@ def translate(text, dictionary):
     for k, v in dictionary.iteritems():
         text = text.replace(k.lower(), v)
     return text
-
+#
+def lookup(word, db):
+    try:
+        ipa, success = db.get(word), True
+    except ValueError:
+        ### lookup from dynamo db
+        success, ipa = word2ipa(word)
+    return success, ipa
 
 def lambda_handler(event, context):
     words = event.get("text")
     if not words:
         return {"error": "missing parameter text"}
     dictionary = {}
-    #response = {"successFactor": 0}
+    db = DynamoDBConnector()
     wordsSet = set(map(removeSpecialChars, words.split(" ")))
     for word in wordsSet:
-        success, ipa = word2ipa(word)
+        success, ipa = lookup(word, db)
         if not success:
             word = word.lower()
-            success, ipa = word2ipa(word)
+            success, ipa = lookup(word, db)
         if success:
             dictionary[word] = ipa
+            db.put(word, ipa)
 
     return {"translation": translate(words, dictionary), "dictionary": dictionary}
 
